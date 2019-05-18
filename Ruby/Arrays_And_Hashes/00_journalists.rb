12e46386533842905df6f7717874606fe63cb9d8
ggoachet <goachetgerard@gmail.com>
2019-05-19 00:26:48 +0200

    Added even more Ruby exercises

---
+++ Ruby/Arrays_And_Hashes/00_journalists.rb
@@ -0,0 +1,248 @@
+### Array
+journalist = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+### Menu
+system "clear"
+puts "
+
+                        ██████╗ ██╗███████╗███╗   ██╗██╗   ██╗███████╗███╗   ██╗██╗   ██╗███████╗    ██╗                                
+                        ██╔══██╗██║██╔════╝████╗  ██║██║   ██║██╔════╝████╗  ██║██║   ██║██╔════╝    ██║                                
+                        ██████╔╝██║█████╗  ██╔██╗ ██║██║   ██║█████╗  ██╔██╗ ██║██║   ██║█████╗      ██║                                
+                        ██╔══██╗██║██╔══╝  ██║╚██╗██║╚██╗ ██╔╝██╔══╝  ██║╚██╗██║██║   ██║██╔══╝      ╚═╝                                
+                        ██████╔╝██║███████╗██║ ╚████║ ╚████╔╝ ███████╗██║ ╚████║╚██████╔╝███████╗    ██╗                                
+                        ╚═════╝ ╚═╝╚══════╝╚═╝  ╚═══╝  ╚═══╝  ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝    ╚═╝                                
+                                                                                                                                        
+ ██████╗██╗  ██╗ ██████╗ ██╗███████╗██╗    ████████╗ █████╗      ██████╗ ██╗   ██╗███████╗███████╗████████╗██╗ ██████╗ ███╗   ██╗    ██╗
+██╔════╝██║  ██║██╔═══██╗██║██╔════╝██║    ╚══██╔══╝██╔══██╗    ██╔═══██╗██║   ██║██╔════╝██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║    ██║
+██║     ███████║██║   ██║██║███████╗██║       ██║   ███████║    ██║   ██║██║   ██║█████╗  ███████╗   ██║   ██║██║   ██║██╔██╗ ██║    ██║
+██║     ██╔══██║██║   ██║██║╚════██║██║       ██║   ██╔══██║    ██║▄▄ ██║██║   ██║██╔══╝  ╚════██║   ██║   ██║██║   ██║██║╚██╗██║    ╚═╝
+╚██████╗██║  ██║╚██████╔╝██║███████║██║       ██║   ██║  ██║    ╚██████╔╝╚██████╔╝███████╗███████║   ██║   ██║╚██████╔╝██║ ╚████║    ██╗
+ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝╚══════╝╚═╝       ╚═╝   ╚═╝  ╚═╝     ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝    ╚═╝
+                                                                                                                                        "
+puts "
+ 
+ +-+-+-+-+ +-+ 
+ |T|a|p|e| |1| pour savoir : Combien il y a de journalistes dans cet array ?
+ +-+-+-+-+ +-+ 
+ +-+-+-+-+ +-+ 
+ |T|a|p|e| |2| pour savoir : Combien d'handles contiennent un numéro ?
+ +-+-+-+-+ +-+ 
+ +-+-+-+-+ +-+ 
+ |T|a|p|e| |3| pour savoir : Combien d'handle contiennent les 4 lettres du prénom `Aude` à la suite (sans prendre en compte les majuscules) ?
+ +-+-+-+-+ +-+ 
+ +-+-+-+-+ +-+ 
+ |T|a|p|e| |4| pour savoir : Combien commencent par une majuscule (première lettre juste après le @) ?
+ +-+-+-+-+ +-+ 
+ +-+-+-+-+ +-+ 
+ |T|a|p|e| |5| pour savoir : Combien contiennent une majuscule ?
+ +-+-+-+-+ +-+ 
+ +-+-+-+-+ +-+ 
+ |T|a|p|e| |6| pour savoir : Combien y a-t-il de underscore _ dans tous les pseudos confondus ?
+ +-+-+-+-+ +-+ 
+ +-+-+-+-+ +-+ 
+ |T|a|p|e| |7| pour savoir : Trie la liste de handle par ordre alphabétique.
+ +-+-+-+-+ +-+ 
+ +-+-+-+-+ +-+ 
+ |T|a|p|e| |8| pour savoir : Quels sont les 50 handles les plus courts de ce array ?
+ +-+-+-+-+ +-+ 
+ +-+-+-+-+ +-+ 
+ |T|a|p|e| |9| pour savoir : Quelle est la position dans l'array de la personne @epenser ?
+ +-+-+-+-+ +-+ 
+
+!!! En cas d'oubli, tapes '0' pour revoir les questions !!!
+
+"
+#loop du menu
+loop do
+
+	print "    	Tape ici ton choix : "
+	menu = gets.to_i
+
+	case menu
+
+	when menu=0
+		system"clear"
+		puts "
+	 Revoici les questions :
+
+	 +-+-+-+-+ +-+ 
+	 |T|a|p|e| |1| pour savoir : Combien il y a de journalistes dans cet array ?
+	 +-+-+-+-+ +-+ 
+	 +-+-+-+-+ +-+ 
+	 |T|a|p|e| |2| pour savoir : Combien d'handles contiennent un numéro ?
+	 +-+-+-+-+ +-+ 
+	 +-+-+-+-+ +-+ 
+	 |T|a|p|e| |3| pour savoir : Combien d'handle contiennent les 4 lettres du prénom `Aude` à la suite (sans prendre en compte les majuscules) ?
+	 +-+-+-+-+ +-+ 
+	 +-+-+-+-+ +-+ 
+	 |T|a|p|e| |4| pour savoir : Combien commencent par une majuscule (première lettre juste après le @) ?
+	 +-+-+-+-+ +-+ 
+	 +-+-+-+-+ +-+ 
+	 |T|a|p|e| |5| pour savoir : Combien contiennent une majuscule ?
+	 +-+-+-+-+ +-+ 
+	 +-+-+-+-+ +-+ 
+	 |T|a|p|e| |6| pour savoir : Combien y a-t-il de underscore _ dans tous les pseudos confondus ?
+	 +-+-+-+-+ +-+ 
+	 +-+-+-+-+ +-+ 
+	 |T|a|p|e| |7| pour savoir : Trie la liste de handle par ordre alphabétique.
+	 +-+-+-+-+ +-+ 
+	 +-+-+-+-+ +-+ 
+	 |T|a|p|e| |8| pour savoir : Quels sont les 50 handles les plus courts de ce array ?
+	 +-+-+-+-+ +-+ 
+	 +-+-+-+-+ +-+ 
+	 |T|a|p|e| |9| pour savoir : Quelle est la position dans l'array de la personne @epenser ?
+	 +-+-+-+-+ +-+ 
+
+		"
+	when menu=1
+			system "clear"
+			puts "########## QUESTION 1 ##########"
+			puts "Combien y a-t-il de journalistes dans ce array ?" 
+			journalistes = journalist.length
+			puts ""
+			puts " Il y a #{journalistes} journalistes dans cet array."
+			puts "
+			"
+
+	when menu=2
+			system "clear"
+			puts "########## QUESTION 2 ##########"
+			puts "Combien d'handle contiennent un numéro ?"
+			handleNum = journalist.count {|a| a.match /[0-9]/ }
+			puts ""
+			puts " Il y a #{handleNum} handles avec un numéro."
+			puts "
+			"
+
+	when menu=3
+			system "clear"
+			puts "########## QUESTION 3 ##########"
+			puts "Combien d'handle contiennent les 4 lettres du prénom 'Aude' à la suite (sans prendre en compte les majuscules) ?"
+			aude = journalist.count {|a| a.match (/(aude)/i) }
+			puts ""
+			puts " Il y a #{aude} handles qui contiennent le prenom `Aude` a la suite sans prendre en compte les majuscules"
+			puts " Voici ces journalistes:"
+			journalist.each do |journ|
+				print " ->" + journ + " " if journ=~ /aude/i
+			end
+			puts "
+			"
+
+	when menu=4
+			system "clear"
+			puts "########## QUESTION 4 ##########"
+			puts "Combien commencent par une majuscule (première lettre juste après le @) ?"
+			puts ""
+			majA = journalist.count {|a| a.match (/\A@[A-Z]/)}
+			puts " Il y a #{majA} handles avec une majuscule juste après le @." 
+			puts "
+			"
+
+	when menu=5
+			system "clear"
+			puts "########## QUESTION 5 ##########"
+			puts "Combien contiennent une majuscule ?"
+			countOnemaj = journalist.count {|a| a.match (/[[:upper:]]{1}/)}
+			puts ""
+			puts " Il y a #{countOnemaj} handles avec une majuscule"
+			puts "
+			"
+
+	when menu=6
+			system "clear"
+			puts "########## QUESTION 6 ##########"
+			puts "Combien y a-t-il de underscore dans tous les pseudos confondus ?"
+			countAnyunder = journalist.count {|a| a.match (/_+/) }
+			puts ""
+			puts " Il y a #{countAnyunder} underscore dans tous les pseudos confondus."
+			puts "
+			"
+
+	when menu=7
+			system "clear"
+			puts "########## QUESTION 7 ##########"
+			puts "Trie la liste de handle par ordre alphabétique."
+			sortJournalistes = journalist.sort
+			puts "Voici la liste par ordre alphabétique: "
+			puts sortJournalistes
+			puts "
+			"
+
+	when menu=8
+			system "clear"
+			puts "########## QUESTION 8 ##########"
+			puts "Quels sont les 50 handles les plus courts de ce array ?"
+			fiftyHandles = journalist.sort_by {|a| a.length}
+			fiftyHandles = fiftyHandles.slice(0...50)
+			puts ""
+			puts " Voici les #{fiftyHandles.length} handles les plus petits :"
+			puts ""
+			puts fiftyHandles.sort
+			puts "
+			"
+
+	when menu=9	
+			system "clear"
+			puts "########## QUESTION 9 ##########"
+			puts "Quelle est la position dans l'array de la personne @epenser ?"
+			thisGuy = journalist.index("@epenser")
+			puts ""
+			puts " Voici sa position dans l'array: #{thisGuy}"
+			puts "
+			"
+	else
+			############################## message d'erreur si input is wrong
+			system "clear"
+			puts "
+
+	                                                          
+	──────────────────────────────────────────                
+	                                                          
+	  ┌─┐┬─┐┬─┐┌─┐┬ ┬┬─┐  ┌─┐┌─┐┌┬┐┌─┐┬  ┌─┐                  
+	  ├┤ ├┬┘├┬┘├┤ │ │├┬┘  ├┤ ├─┤ │ ├─┤│  ├┤                   
+	  └─┘┴└─┴└─└─┘└─┘┴└─  └  ┴ ┴ ┴ ┴ ┴┴─┘└─┘                  
+	                                                          
+	──────────────────────────────────────────                
+	                                                          
+	╔═╗┬─┐╦ ╦                                                 
+	╠═╝├┬┘╚╦╝  --Ver01.25.85                                               
+	╩  ┴└─ ╩   --Carenado CT210 Centurion II V3/NACA_16_Prop_INT.afl
+
+		>/......
+
+		PrY has failed :
+
+		ini FAIL OVER BOOT 
+
+		Displaying system logs:
+
+	──────────────────────────────────────────  
+
+	   ╦╔═╗╔═╗       ┬  ┬┌─┐┬─┐┌─┐┬┌─┐┌┐┌                     
+	───║║  ║    ───  └┐┌┘├┤ ├┬┘└─┐││ ││││                     
+	   ╩╚═╝╚═╝        └┘ └─┘┴└─└─┘┴└─┘┘└┘                     
+	      ┌─┐┌─┐┬┬  ┌─┐┌┬┐                                    
+	──────├┤ ├─┤││  ├┤  ││                                    
+	      └  ┴ ┴┴┴─┘└─┘─┴┘                                    
+	   ╦╔═╗╔═╗       ┬─┐┌─┐┌┐ ┌─┐┌─┐┌┬┐                       
+	───║║  ║    ───  ├┬┘├┤ ├┴┐│ ││ │ │                        
+	   ╩╚═╝╚═╝       ┴└─└─┘└─┘└─┘└─┘ ┴                        
+	      ┌─┐┌─┐┬┬  ┌─┐┌┬┐                                    
+	──────├┤ ├─┤││  ├┤  ││                                    
+	      └  ┴ ┴┴┴─┘└─┘─┴┘                                    
+	                                                          
+	──────────────────────────────────────────                
+	                                                          
+	╔═╗┬  ┌─┐┌─┐┌─┐┌─┐  ┌─┐┬ ┬┌─┐┌─┐┬┌─  ┬ ┬┌─┐┬ ┬┬─┐  ╦╔═╗╔═╗
+	╠═╝│  ├┤ ├─┤└─┐├┤   │  ├─┤├┤ │  ├┴┐  └┬┘│ ││ │├┬┘  ║║  ║  
+	╩  ┴─┘└─┘┴ ┴└─┘└─┘  └─┘┴ ┴└─┘└─┘┴ ┴   ┴ └─┘└─┘┴└─  ╩╚═╝╚═╝
+                                                          
+	──────────────────────────────────────────                
+
+	(aifl.cpp:112 - DEC 05 2004 - All rights reserved)
+
+	                                         "
+#loop end
+	break
+end
+end
---
+++ Ruby/Arrays_And_Hashes/README.md
@@ -0,0 +1,17 @@
+# Bonjour !
+
+Dépôt du projet traitant des Arrays et les Hash !
+
+## Utilisation:
+- Lancer le terminal et taper:
+- ruby nom_appli.rb
+
+## Description des fichiers:
+
+* 00_journalists
+
+	> Utilise un array de pseudonymes twitter et utilise des méthodes Ruby et les expressions régulières pour le manipuler.
+
+* 01_cryptocurrencies
+	
+	> Assemble deux arrays en un hash et utilise des méthodes Ruby et les expressions régulières pour le manipuler.
